d29ac08cf88a12e6baac23bab31a4c75f7ffe19e
YuSiu <jamesjames69116911@yahoo.com.tw>
2022-12-04 19:52:22 +0800

    cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,9 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3000/Login',
     'https://longing-head-production.up.railway.app/Login',
-    'https://longing-head-production.up.railway.app'
+    'https://longing-head-production.up.railway.app',
+    'https://reactproject3client-production.up.railway.app/',
+    'https://reactproject3client-production.up.railway.app/Article',
 ];
 
 module.exports = allowedOrigins;
